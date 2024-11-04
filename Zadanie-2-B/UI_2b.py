fc5e86ed246c82b331944ca547a9b6814fff1df0
jfridrich <jakub.fridrich1@gmail.com>
2024-11-04 23:52:29 +0100

    pridanie kcentroid

--- Zadanie-2-B/UI_2b.py
+++ Zadanie-2-B/UI_2b.py
@@ -23,8 +23,8 @@ MAX_X, MAX_Y=5000,5000
 MIN_X, MIN_Y=-5000,-5000
 
 array_ran_sur=[]
-num_points=2500
-num_clus=5
+num_points=40000
+num_clus=15
 radius=1
 scaling_down=20
 distances=np.zeros((num_points+20, num_clus))
@@ -38,9 +38,9 @@ def init_20(array,radius,scale):
         ran_sur=[ran_x,ran_y]
         array.append(ran_sur)
 
-        coord_x=(ran_x//scale)+250+50   #+50 kvoli okraju
-        coord_y=(ran_y//scale)+250+50
-        canvas.create_oval(coord_x-radius,coord_y-radius,coord_x+radius,coord_y+radius, fill='black')
+        #coord_x=(ran_x//scale)+250+50   #+50 kvoli okraju
+        #coord_y=(ran_y//scale)+250+50
+        #canvas.create_oval(coord_x-radius,coord_y-radius,coord_x+radius,coord_y+radius, fill='black')
     #print(array_ran_sur)
 
 
@@ -68,33 +68,34 @@ def generate_more(arr, count,radius,scale):
 
     arr.append(new)
 
-    coord_x=(new[0]//scale)+250+50   #+50 kvoli okraju
-    coord_y=(new[1]//scale)+250+50
-
-    canvas.create_oval(coord_x-radius,coord_y-radius,coord_x+radius,coord_y+radius, fill="black", outline="")
+    #coord_x=(new[0]//scale)+250+50   #+50 kvoli okraju
+    #coord_y=(new[1]//scale)+250+50
+    #canvas.create_oval(coord_x-radius,coord_y-radius,coord_x+radius,coord_y+radius, fill="black", outline="")
     return new
 
+#------------------------------------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------------------------------------------------
 #vyber medoidov nahodne
-def kmetoid_draw(arr, k, radius, scale,medoids_id):
-    medoids_sample=random.sample(arr,k)
+def kmeans_draw(arr, k, radius, scale,centers_id):
+    centers_sample=random.sample(arr,k)
     for num in range(k):
-        medoids_id.append(medoids_sample[num])
+        centers_id.append(centers_sample[num])
 
     #print(f"zoznam medoidov: {medoids_id}")
 
-    for i in range(k):
-        coord_x=(medoids_sample[i][0]//scale)+250+50   #+50 kvoli okraju
-        coord_y=(medoids_sample[i][1]//scale)+250+50
-        canvas.create_oval(coord_x-radius,coord_y-radius,coord_x+radius,coord_y+radius, width=5, outline="#24fc03")
+    """for i in range(k):
+        coord_x=(centers_sample[i][0]//scale)+250+50   #+50 kvoli okraju
+        coord_y=(centers_sample[i][1]//scale)+250+50
+        canvas.create_oval(coord_x-radius,coord_y-radius,coord_x+radius,coord_y+radius, width=5, outline="#24fc03")"""
 
 #vypocet vzdialenosti bodov od medoidov, distances => maxtrix POINTS x MEDOIDS
-def dist_calc(distances, array_m, array_points):
+def dist_calc(distances, array_centers, array_points):
     #print(len(array_points))
     #print(len(array_m))
 
     for i in range(len(array_points)):
-        for j in range(len(array_m)):
-            medoid=np.array([array_m[j][0],array_m[j][1]])
+        for j in range(len(array_centers)):
+            medoid=np.array([array_centers[j][0],array_centers[j][1]])
             point=np.array([array_points[i][0],array_points[i][1]])
             distances[i,j]=round(np.linalg.norm(medoid-point),2)
 
@@ -119,7 +120,7 @@ def update_med(new_medoids, clusters, color, iter):
         #print(f"{new_medoids}")
 
         if iter==10:
-            color="lightgreen"
+            color="cyan"
             coord_x=(new_medoid[0]//20)+250+50   #+50 kvoli okraju
             coord_y=(new_medoid[1]//20)+250+50
             canvas.create_oval(coord_x-radius, coord_y-radius, coord_x+radius, coord_y+radius, width=5, outline=color)
@@ -129,43 +130,44 @@ def update_med(new_medoids, clusters, color, iter):
 
 
 
+def update_cent(clusters, iter):
+    new_centroids = []
+    for cluster in clusters:
+        if cluster:  # Skontroluj, či klaster nie je prázdny
+            centroid = np.mean(cluster, axis=0).tolist()
+            #print(centroid)
+            new_centroids.append(centroid)
+        else:
+            # Ak je klaster prázdny, pridaj pôvodný centroid
+            new_centroids.append([0, 0])  # Môžeš nahradiť túto hodnotu inak, podľa potreby
+
+    return new_centroids
 
 
 
-def calculate_average_distance(clusters, medoids):
+def calculate_average_distance(clusters, centers):
     global array_worst_dist
     #print(f"v clusters je {len(clusters)} klasterov")
     for c_index, cluster in enumerate(clusters):
-        medoid = np.array(medoids[c_index])
-        distances = [np.linalg.norm(np.array(point) - medoid) for point in cluster]
+        center = np.array(centers[c_index])
+        distances = [np.linalg.norm(np.array(point) - center) for point in cluster]
         average_distance = np.mean(distances)
+
         if average_distance > 500:
-            #print(average_distance)
             array_worst_dist.append(average_distance)
             return False  # Ak je priemerná vzdialenosť v niektorom klastri > 500, pokračuj v iterácii
     return True  # Ak sú všetky klastre v poriadku, ukončíme cyklus
 
 
-
 # Hlavný cyklus
-def iterative_clustering():
+def kmed_clustering():
     global medoids_id, new_medoids, clusters, num_clus, num_points, array_worst_dist
 
     iteration_count = 0
-    color="cyan"
-    while iteration_count!=10:
+    color="black"
+    while iteration_count!=20:
         iteration_count += 1
         print(f"Iterácia: {iteration_count}")
-        """if(iteration_count==0):
-            color="yellow"
-        elif(iteration_count==1):
-            color="orange"
-        elif(iteration_count==2):
-            color="red"
-        elif (10>iteration_count>3):
-            color="blue"
-        elif (iteration_count==10):
-            color="cyan"""
 
         # Krok 1: Vypočítaj vzdialenosti a priraď body k najbližším medoidom
         dist_calc(distances, medoids_id, array_ran_sur)
@@ -185,6 +187,72 @@ def iterative_clustering():
             #print("Podmienka nesplnená: Opakujeme klastrovanie.")
 
 
+def draw_clusters(clusters):
+    for cluster_index, cluster in enumerate(clusters):
+        color = cluster_colors[cluster_index % len(cluster_colors)]
+        for point in cluster:
+
+            coord_x = (point[0] // scaling_down) + 250 + 50
+            coord_y = (point[1] // scaling_down) + 250 + 50
+            canvas.create_oval(coord_x - radius, coord_y - radius, coord_x + radius, coord_y + radius, fill=color, outline="")
+
+        #print(cluster[cluster_index])
+        coord_x_center=(cluster[cluster_index][0] // scaling_down) + 250 + 50
+        coord_y_center=(cluster[cluster_index][1] // scaling_down) + 250 + 50
+        canvas.create_oval(coord_x_center - 3, coord_y_center - 3, coord_x_center + 3, coord_y_center + 3, fill=color, outline="black")
+
+
+def kcent_clustering():
+    global centroids_id, clusters, num_clus, array_ran_sur
+    iteration_count = 0
+    
+    # Počiatočná inicializácia centroidov
+    centroids_id = random.sample(array_ran_sur, num_clus)
+    
+    while iteration_count < 20:
+        iteration_count += 1
+        print(f"Iterácia: {iteration_count}")
+        
+        # Krok 1: Vypočítaj vzdialenosti medzi každým bodom a centroidmi
+        dist_calc(distances, centroids_id, array_ran_sur)
+        
+        # Krok 2: Priraď body k najbližším centroidom
+        clusters = [[] for _ in range(num_clus)]
+        clustering(distances, clusters, array_ran_sur)
+
+        # Krok 3: Aktualizácia centroidov
+        new_centroids = update_cent(clusters, iteration_count)
+        
+        # Skontroluj, či sa centroidy zmenili; ak nie, ukonči cyklus
+        if new_centroids == centroids_id:
+            print("Centroidy sa stabilizovali.")
+            break
+        
+        centroids_id = new_centroids  # Aktualizácia centroidov pre ďalšiu iteráciu
+
+        # Krok 4: Skontroluj priemerné vzdialenosti (voliteľné)
+        if calculate_average_distance(clusters, centroids_id):
+            break  # Ukonči cyklus, ak sú všetky priemerné vzdialenosti v poriadku
+
+    draw_clusters(clusters)
+
+
+
+
+#----------------------------------------------------------------------------------------------------------------------------------------
+centroids_id=[]
+medoids_id=[]
+
+new_medoids=[]
+new_centroids=[]
+
+array_worst_dist=[]
+
+cluster_colors = [
+    "red", "blue", "green", "purple", "orange", "yellow", "pink", "cyan", "magenta", "brown",
+    "lime", "indigo", "teal", "gold", "lightblue", "coral", "darkgreen", "salmon", "violet", "khaki"
+]
+
 start=time.time()
 #inicializacia prvych 20 bodov a dodatocnych 40k bodov
 init_20(array_ran_sur,radius,scaling_down);
@@ -193,17 +261,24 @@ for count in range(num_points):
     generate_more(array_ran_sur, count+1,radius,scaling_down)
 num_points+=20
 
-#-----------------------------------------------
-#moznost k-means s centrom medoid
-medoids_id=[]
-new_medoids=[]
-array_worst_dist=[]
-
-kmetoid_draw(array_ran_sur,num_clus,radius,scaling_down,medoids_id)
-iterative_clustering()
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+kmeans_draw(array_ran_sur,num_clus,radius,scaling_down,centroids_id)
+kcent_clustering()
 print(array_worst_dist)
-#-----------------------------------------------
+
+"""kmeans_draw(array_ran_sur,num_clus,radius,scaling_down,centroids_id)
+dist_calc(distances, centroids_id, array_ran_sur)
+clusters = [[] for _ in range(num_clus)]
+clustering(distances, clusters, array_ran_sur)
+new_medoids = []
+update_cent(clusters)"""
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+"""kmetoid_draw(array_ran_sur,num_clus,radius,scaling_down,medoids_id)
+kmed_clustering()
+print(array_worst_dist)"""
+#------------------------------------------------------------------------------
 end=time.time()
 print(f"cas bezania: {end-start}")
-
 root.mainloop();
